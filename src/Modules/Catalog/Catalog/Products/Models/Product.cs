eb0b334ecaa143d53508cc8166f352822ea59dc2
hkhaled <hkhaled@DEV-001>
2026-03-30 11:19:12 +0200

    add GlobalUsing.cs in catalog module

--- src/Modules/Catalog/Catalog/Products/Models/Product.cs
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -1,14 +1,12 @@
-﻿using Shared.DDD;
-
-namespace Catalog.Products.Models
+﻿namespace Catalog.Products.Models
 {
     public class Product : Entity<Guid>
     {
-        public string Name { get; set; } = default!;
-        public List<string> Categories { get; set; } = new();
-        public string Description { get; set; } = default!;
-        public string ImageFile { get; set; } = default!;
-        public decimal Price { get; set; }
+        public string Name { get; private set; } = default!;
+        public List<string> Categories { get; private set; } = new();
+        public string Description { get; private set; } = default!;
+        public string ImageFile { get; private set; } = default!;
+        public decimal Price { get; private set; }
 
         public static Product Create(Guid id ,string name,  List<string> categories, string description, string imageFile, decimal price)
         {
@@ -24,6 +22,22 @@ namespace Catalog.Products.Models
                 ImageFile = imageFile,
                 Price = price
             };
+
+            return product;
+        }
+
+        public void Update(Guid id, string name, List<string> categories, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            Name = name;
+            Categories = categories;
+            Description = description;
+            ImageFile = imageFile;
+            Price = price;
+
+
         }
     }
 }
