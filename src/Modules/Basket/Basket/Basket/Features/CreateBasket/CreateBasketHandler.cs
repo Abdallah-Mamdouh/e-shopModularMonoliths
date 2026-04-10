70af7f28de71eecac3a9aefab1ccb63e28ea9c73
Abdallah-Mamdouh <119abdallah118@gmail.com>
2026-04-10 11:39:47 +0200

    feat(basket): add CreateBasket command and handler

---
+++ src/Modules/Basket/Basket/Basket/Features/CreateBasket/CreateBasketHandler.cs
@@ -0,0 +1,13 @@
+﻿using Basket.Basket.Dtos;
+using Shared.CQRS;
+
+namespace Basket.Basket.Features.CreateBasket
+{
+    public record CreateBasketCommand(ShoppingCartDto ShoppingCart) : ICommand<CreateBasketResult>;
+
+    public record CreateBasketResult(Guid Id);
+
+    public class CreateBasketHandler
+    {
+    }
+}
